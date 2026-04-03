6b1882d7fdd663ebcaa3de6afb1e6353c46d4313
tristan-tr <t.rietveldt@student.tudelft.nl>
2026-04-03 23:14:24 +0200

    geyser range buff

---
+++ patches/statchange/GeyserPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace mq_mod_2.patches.statchange;
+
+[HarmonyPatch(typeof(GeyserObject), MethodType.Constructor)]
+public class GeyserPatch
+{
+    static void Postfix(GeyserObject __instance, ref float ___RADIUS)
+    {
+        ___RADIUS = 8f;
+    }
+}
