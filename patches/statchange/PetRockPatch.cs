8bdd7c4eeb6ac63cc887dc7d40ecb34ab35f33e8
tristan-tr <t.rietveldt@student.tudelft.nl>
2026-04-03 23:12:29 +0200

    make pet rock a bit faster

---
+++ patches/statchange/PetRockPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace mq_mod_2.patches.statchange;
+
+[HarmonyPatch(typeof(PetRockObject), MethodType.Constructor)]
+public class PetRockPatch
+{
+    static void Postfix(ref float ___POWER)
+    {
+        ___POWER = 30f;
+    }
+
+}
