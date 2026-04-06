59b6f4f1e6b81eef82346e4a4d1ec15c1f963ca6
tristan-tr <t.rietveldt@student.tudelft.nl>
2026-04-06 18:08:43 +0200

    compound more damage

---
+++ patches/balance/custom/CompoundPatch.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using UnityEngine;
+
+namespace mq_mod_2.patches.balance.custom;
+
+[HarmonyPatch]
+public static class CompoundPatch
+{
+    [HarmonyPatch(typeof(CompoundObject), "Init")]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> CompoundObjectInitTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        // Change damage scaling from 3f to 4f
+        // Logic: float num2 = this.POWER + (float)num * 12f;
+        // Logic: float num3 = this.DAMAGE + (float)num * 3f;
+        matcher.Start().MatchForward(false,
+                new CodeMatch(i => i.LoadsConstant(3f)),
+                new CodeMatch(OpCodes.Mul),
+                new CodeMatch(OpCodes.Add)
+            )
+            .ThrowIfInvalid("Could not find damage scaling (num * 3f)")
+            .SetOperandAndAdvance(4f);
+
+        return matcher.InstructionEnumeration();
+    }
+
+    [HarmonyPatch(typeof(CompoundObject), nameof(CompoundObject.Init))]
+    [HarmonyPrefix]
+    public static void CompoundObjectInitPrefix(CompoundObject __instance)
+    {
+        __instance.DAMAGE = 8f;
+    }
+}
