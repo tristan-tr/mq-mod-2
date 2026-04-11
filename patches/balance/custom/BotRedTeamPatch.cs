269067ee3677ef02a403aa75f11baa0592e5f768
tristan-tr <t.rietveldt@student.tudelft.nl>
2026-04-11 20:59:34 +0200

    bots red

---
+++ patches/balance/custom/BotRedTeamPatch.cs
@@ -0,0 +1,51 @@
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace mq_mod_2.patches.balance.custom
+{
+    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.DetermineBots))]
+    public static class PlayerManager_DetermineBots_Patch
+    {
+        public static void Postfix()
+        {
+            foreach (var player in PlayerManager.players.Values)
+            {
+                if (player != null && player.inputType == InputType.AI)
+                {
+                    player.teamColor = TeamColor.Red;
+                    player.teamName = "Red Team";
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.DetermineBotsForOnlineUI))]
+    public static class PlayerManager_DetermineBotsForOnlineUI_Patch
+    {
+        public static bool Prefix(ref List<TeamColor> __result)
+        {
+            __result = new List<TeamColor>();
+            for (int i = 0; i < PlayerManager.gameSettings.numberOfBots; i++)
+            {
+                __result.Add(TeamColor.Red);
+            }
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.AddPlayer))]
+    public static class NetworkManager_AddPlayer_Patch
+    {
+        public static void Postfix(int index)
+        {
+            if (PlayerManager.players.TryGetValue(index, out Player player))
+            {
+                if (player.inputType == InputType.AI)
+                {
+                    player.teamColor = TeamColor.Red;
+                    player.teamName = "Red Team";
+                }
+            }
+        }
+    }
+}
