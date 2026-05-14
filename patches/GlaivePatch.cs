de300528e476d7f0217e255d83e7f3caf859b4ee
tristan-tr <t.rietveldt@student.tudelft.nl>
2026-05-14 13:28:52 +0200

    Glaive cooldown 4s to 3s

--- patches/GlaivePatch.cs
+++ patches/GlaivePatch.cs
@@ -2,12 +2,24 @@
 
 namespace mq_mod_2.patches;
 
-[HarmonyPatch(typeof(GlaiveObject), nameof(GlaiveObject.Init))]
-public class GlaivePatch
+[HarmonyPatch]
+public static class GlaivePatch
 {
-    static void Prefix(GlaiveObject __instance, ref float ___POWER)
+    [HarmonyPatch(typeof(GlaiveObject), nameof(GlaiveObject.Init))]
+    [HarmonyPrefix]
+    static void InitPrefix(GlaiveObject __instance, ref float ___POWER)
     {
         __instance.DAMAGE = 8f;
         ___POWER = 38f;
+    }
+
+    [HarmonyPatch(typeof(SpellManager), "Awake")]
+    [HarmonyPostfix]
+    public static void SpellManagerAwakePostfix(SpellManager __instance)
+    {
+        if (__instance.spell_table != null && __instance.spell_table.TryGetValue(SpellName.Glaive, out Spell glaive))
+        {
+            glaive.cooldown = 3f;
+        }
     }
 }
