56fa1bac5ff4d6c1a485dd9341fb959d4d1f1757
tristan-tr <t.rietveldt@student.tudelft.nl>
2026-05-14 13:50:59 +0200

    lightning bug speed x1.5

---
+++ patches/LightningBugPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace mq_mod_2.patches;
+
+[HarmonyPatch(typeof(LightningBugObject), nameof(LightningBugObject.Init))]
+public class LightningBugPatch
+{
+    static void Prefix(ref float velocity)
+    {
+        velocity *= 1.5f;
+    }
+}
