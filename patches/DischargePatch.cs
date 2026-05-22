4ef85ce795b1869c2fd538c7693d989c4708f863
tristan-tr <t.rietveldt@student.tudelft.nl>
2026-05-22 16:53:13 +0200

    discharge -50% knockback

--- patches/DischargePatch.cs
+++ patches/DischargePatch.cs
@@ -8,22 +8,34 @@ namespace mq_mod_2.patches;
 
 public static class DischargePatch
 {
-    [HarmonyPatch(typeof(WizardStatus), nameof(WizardStatus.rpcApplyDamage))]
-    [HarmonyTranspiler]
-    public static IEnumerable<CodeInstruction> rpcApplyDamageTranspiler(IEnumerable<CodeInstruction> instructions)
-    {
-        // Change 0.5f (50% reduction) to 0.33333334f (2/3rds reduction)
-        return instructions.ReplaceConstant(0.5f, 0.33333334f);
-    }
+    // [HarmonyPatch(typeof(WizardStatus), nameof(WizardStatus.rpcApplyDamage))]
+    // [HarmonyTranspiler]
+    // public static IEnumerable<CodeInstruction> rpcApplyDamageTranspiler(IEnumerable<CodeInstruction> instructions)
+    // {
+    //     // Change 0.5f (50% reduction) to 0.33333334f (2/3rds reduction)
+    //     return instructions.ReplaceConstant(0.5f, 0.33333334f);
+    // }
 
     [HarmonyPatch(typeof(PhysicsBody), nameof(PhysicsBody.rpcAddForce))]
-    [HarmonyPrefix]
-    public static void rpcAddForcePrefix(PhysicsBody __instance, ref Vector3 impulse)
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> rpcAddForceTranspiler(IEnumerable<CodeInstruction> instructions)
     {
-        var status = __instance.GetComponent<WizardStatus>();
+        return new CodeMatcher(instructions)
+            .Start()
+            .InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldarga, 1),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DischargePatch), nameof(ModifyImpulse)))
+            )
+            .InstructionEnumeration();
+    }
+
+    public static void ModifyImpulse(PhysicsBody instance, ref Vector3 impulse)
+    {
+        var status = instance.GetComponent<WizardStatus>();
         if (status != null && status.dischargeCount > 0)
         {
-            impulse *= 0.5f; // 50% knockback reduction
+            impulse *= 0.5f;
         }
     }
 }
